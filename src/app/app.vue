1b8b82f3ac131c55b8ade4c9ad0280cd0401ee06
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2023-06-29 15:07:47 +0300

    Added basic CSS variable set

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>Hello World!</div>
+  <div>
+		<div :class="containerClassList">
+			<button @click="setTheme('brown')">Brown</button>
+			<button @click="setTheme('green')">Green</button>
+			<button @click="setTheme('violet')">Violet</button>
+		</div>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			theme: 'brown'
+		}
+	},
+	methods: {
+		setTheme(theme) {
+			this.theme = theme
+		}
+	},
+	computed: {
+		containerClassList() {
+			return `container ${this.theme} flex`
+		}
+	}
+}
+</script>
 
 <style lang="scss">
 @import './app';
